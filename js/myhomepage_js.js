915e6582ae3f38edd74721259f0a76b3129f3ad0
gabrielzschmitz <gabrielzschmitz@gmail.com>
2020-10-19 00:31:35 -0300

    Some Bug Fixes

--- js/myhomepage_js.js
+++ js/myhomepage_js.js
@@ -1,30 +1,27 @@
+setInterval(realtimeClock, 500);
 function realtimeClock() {
 
     // Set variables
     var rtClock = new Date();
     var hours = rtClock.getHours();
     var minutes = rtClock.getMinutes();
-
+    var day = rtClock.getDate();
+    
+    //Setting week day and day in three letters
     var week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     var month = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-    var day = rtClock.getDate();
-
+    
     //Pad the hours, minutes and day with leading zeros
     hours = ("0" + hours).slice(-2);
     minutes = ("0" + minutes).slice(-2);
     day = ("0" + day).slice(-2);
-
-    //Setting week day and day in three letters
     
 
     //Display Clock
     document.getElementById('clock').innerHTML =
         hours + ":" + minutes;
-    var t = setTimeout(realtimeClock,500);
     
     //Display Date
     document.getElementById('date').innerHTML =
         week[rtClock.getDay()] + ", " + month[rtClock.getMonth()] + " " + day;
-    var t = setTimeout(realtimeClock,500);
-
 }
